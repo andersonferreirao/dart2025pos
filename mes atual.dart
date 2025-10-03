5d5da2ee246984b957a478690860137d5ea2c97a
andersonxx <69999132+andersonferreirao@users.noreply.github.com>
2025-10-02 21:41:03 -0300

    mes atual é maiot, menor ou igual?

---
+++ mes atual.dart
@@ -0,0 +1,12 @@
+void main() {
+  // int informado = ?
+  int mesAtual = 11;
+  int mesInformado = 11;
+  if (mesAtual > mesInformado)
+    print('é maior que o mês informado $mesInformado');
+
+  if (mesAtual < mesInformado)
+    print('é maior que o mês informado $mesInformado');
+
+  if (mesAtual == mesInformado) print('é igual ao mês informado $mesInformado');
+}
